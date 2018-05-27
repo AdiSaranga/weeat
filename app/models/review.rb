f7f7dca3c9c9be30419b9b7411ae3bb21630b089
AdiSaranga <adi.saranga@wework.com>
2018-05-27 17:41:03 +0300

    fix review validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
-  validates :name, :cuisine, presence: true
+  validates :name, presence: true
   validates :rating, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5}
 
   belongs_to :restaurant
