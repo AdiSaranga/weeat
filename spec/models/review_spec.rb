d42ecfaf4c1284bbbceefaad0ab3fcb6d0cbf3a5
AdiSaranga <adi.saranga@wework.com>
2018-05-28 12:27:36 +0300

    fixe for heroku

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe Review, type: :model do 
-  # it 'should fail validation on review greater than 5' do
-  #   review = create(:review, rating: 7)
-  # end
-end
+# RSpec.describe Review, type: :model do
+#   # it 'should fail validation on review greater than 5' do
+#   #   review = create(:review, rating: 7)
+#   # end
+# end
